0d5436e38d1b3ada9c05de9f81584d23df896a11
Joohae Kim <joohaek@gmail.com>
2015-10-04 15:30:18 -0700

    Create universal project     - Xcode 6.4 (6E35b)

---
+++ iOSSamples/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSSamples
+//
+//  Created by Joohae Kim on 2015. 10. 4..
+//  Copyright (c) 2015년 Joohae Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSSamples/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOSSamples
+//
+//  Created by Joohae Kim on 2015. 10. 4..
+//  Copyright (c) 2015년 Joohae Kim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
