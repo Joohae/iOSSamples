6f2620a6fb5b552819ceb55c497976b4125e7427
Joohae Kim <joohaek@gmail.com>
2016-03-10 21:00:46 -0800

    Add sub-view controllers

---
+++ snsservice/SNSDeviceBase.h
@@ -0,0 +1,13 @@
+//
+//  SNSDeviceBase.h
+//  iOSSamples
+//
+//  Created by Joohae Kim on 2016. 3. 10..
+//  Copyright © 2016년 Joohae Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SNSDeviceBase : NSObject
+
+@end
