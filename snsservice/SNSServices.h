2952ae6d816343b2ae7d9632d51719baa23cd4ff
Joohae Kim <joohaek@gmail.com>
2016-03-12 17:02:09 -0800

    SNS Services Manager

---
+++ snsservice/SNSServices.h
@@ -0,0 +1,27 @@
+//
+//  SNSServices.h
+//  iOSSamples
+//
+//  Created by Joohae Kim on 2016. 3. 10..
+//  Copyright © 2016년 Joohae Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define SNSServiceTitle         @"TITLE"
+#define SNSServiceIcon          @"ICON"
+#define SNSServiceDescription   @"DESCRIPTION"
+
+typedef NS_ENUM(NSInteger, SNSServicesType) {
+    SNSServiceInstagram,
+    SNSServiceFlickr,
+    SNSServicePicasa,
+    SNSServiceFacebook
+};
+
+@interface SNSServices : NSObject
+
++(NSInteger)numberOfServices;
++(NSDictionary *)getServiceAt:(NSInteger)index;
+
+@end
